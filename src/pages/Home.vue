b6f27db8d34a2c38befe1ee12c30b4cc28708b0c
saslavik <saslavik@gmail.com>
2021-01-20 17:57:42 +0200

    sort added

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -22,7 +22,11 @@
           </div>
           <div class="repos__wrapper" v-if="repos">
             <!-- repo item -->
-            <div class="repo-item" v-for="repo in repos" :key="repo.id">
+            <div class="sort">
+              <span @click="sort('name')">Title &#8595;</span>
+              <span @click="sort('stargazers_count')">Stars⭐&#8595;</span>
+            </div>
+            <div class="repo-item" v-for="repo in reposSort" :key="repo.id">
               <div class="repos-info">
                 <a class="link" target="_blank" :href="repo.html_url">{{ repo.name }}</a>
                 <span>{{ repo.stargazers_count }} ⭐</span>
@@ -45,10 +49,23 @@ export default {
   data() {
     return {
       search: '',
+      currentSort: 'name',
+      currentSortDir: 'asc',
       err: null,
       user: null,
       repos: null,
     };
+  },
+  computed: {
+    reposSort() {
+      return this.repos.slice().sort((a, b) => {
+        let mod = 1;
+        if (this.currentSortDir === 'desc') mod = -1;
+        if (a[this.currentSort] < b[this.currentSort]) return -1 * mod;
+        if (a[this.currentSort] > b[this.currentSort]) return 1 * mod;
+        return 0;
+      });
+    },
   },
   methods: {
     getRepos() {
@@ -67,6 +84,12 @@ export default {
           this.user = null;
           this.err = "Can't find this user";
         });
+    },
+    sort(e) {
+      if (e === this.currentSort) {
+        this.currentSortDir = this.currentSortDir === 'asc' ? 'desc' : 'asc';
+      }
+      this.currentSort = e;
     },
   },
 };
@@ -100,7 +123,15 @@ button {
 .repos__wrapper {
   width: 400px;
   margin: 30px 0;
-
+  .sort {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-bottom: 10px;
+    span {
+      cursor: pointer;
+    }
+  }
 }
 .repos-info {
   display: flex;
